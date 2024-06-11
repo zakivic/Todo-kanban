77d167b0024ef85519215723139e9dad20d44571
zakaria <zakaria.boutarfa@7peakssoftware.com>
2024-06-12 06:23:51 +0700

    updated mains with the traits

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 mod to_do;
+use crate::to_do::traits::delete::Delete;
+use crate::to_do::traits::edit::Edit;
+use crate::to_do::traits::get::Get;
 use to_do::enums::TaskStatus;
 use to_do::to_do_factory;
 use to_do::ItemTypes;
 
 fn main() {
-    let to_do_item = to_do_factory("washing", TaskStatus::DONE);
-    match to_do_item {
+    let to_do_items = to_do_factory("washing", TaskStatus::PENDING);
+    match to_do_items {
         ItemTypes::Done(item) => {
-            println!("{}", item.super_struct.status);
-            println!("{}", item.super_struct.title);
+            item.get(&item.super_struct.title);
+            item.delete(&item.super_struct.title);
         }
         ItemTypes::Pending(item) => {
-            println!("{}", item.super_struct.status);
-            println!("{}", item.super_struct.title);
+            item.get(&item.super_struct.title);
+            item.set_to_done(&item.super_struct.title);
         }
     }
 }

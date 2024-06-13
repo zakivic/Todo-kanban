b451eadbf8802ad69dd54deba514b82ff06501fb
Zakaria <zakivic89@gmail.com>
2024-06-14 05:54:59 +0700

    added read and write to main

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,15 @@
-mod to_do;
-use crate::to_do::traits::delete::Delete;
-use crate::to_do::traits::edit::Edit;
-use crate::to_do::traits::get::Get;
-use to_do::enums::TaskStatus;
-use to_do::to_do_factory;
-use to_do::ItemTypes;
-
+mod state;
+use serde_json::value::Value;
+use serde_json::{json, Map};
+use state::{read_file, write_to_file};
+use std::env;
 fn main() {
-    let to_do_items = to_do_factory("washing", TaskStatus::PENDING);
-    match to_do_items {
-        ItemTypes::Done(item) => {
-            item.get(&item.super_struct.title);
-            item.delete(&item.super_struct.title);
-        }
-        ItemTypes::Pending(item) => {
-            item.get(&item.super_struct.title);
-            item.set_to_done(&item.super_struct.title);
-        }
-    }
+    let args: Vec<String> = env::args().collect();
+    let status: &String = &args[1];
+    let title: &String = &args[2];
+    let mut state: Map<String, Value> = read_file("./state.json");
+    println!("Before operation: {:?}", state);
+    state.insert(title.to_string(), json!(status));
+    println!("After operation: {:?}", state);
+    write_to_file("./state.json", &mut state);
 }

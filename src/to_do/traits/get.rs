38235153119375e0b1f5950d23071ae839c93f64
Zakaria <zakivic89@gmail.com>
2024-06-19 06:16:10 +0700

    updated the get trait to read from json map

--- src/to_do/traits/get.rs
+++ src/to_do/traits/get.rs
@@ -1,5 +1,16 @@
+use serde_json::value::Value;
+use serde_json::Map;
 pub trait Get {
-    fn get(&self, title: &str) {
+    fn get(&self, title: &str, state: &Map<String, Value>) {
+        let item: Option<Value> = state.get("title");
+
+        match item {
+            Some(result) => {
+                println!("\n\nItem: {}", title);
+                println!("Status: {} \n\n", result);
+            }
+            None => println!("item: {} was not found", title),
+        }
         println!("{} is being fetched", title);
     }
 }

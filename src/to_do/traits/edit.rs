dddbe0a02b7330adc5482f39dc80682f0de8808a
Zakaria <zakivic89@gmail.com>
2024-06-20 05:49:36 +0700

    updated the edit crate

--- src/to_do/traits/edit.rs
+++ src/to_do/traits/edit.rs
@@ -1,9 +1,18 @@
-pub trait Edit {
-    fn set_to_done(&self, title: &str) {
-        println!("{} is being set to done", title);
-    }
+use serde_json::jason;
+use serde_json::value::Value;
+use serde_json::Map;
 
-    fn set_to_pending(&self, title: &str) {
-        println!("{} is being set to pending", title);
+use super::super::enums::TaskStatus;
+use create::write_to_file;
+pub trait Edit {
+    fn set_to_done(&self, title: &String, state: &mut Map<String, Value>) {
+        state.insert(title.to_string(), json!(TaskStatus::DONE.stringify()));
+        write_to_file("./state.json", state);
+        println!("\n\n{} is being set to done\n\n", title);
+    }
+    fn set_to_pending(&self, title: &String, state: &mut Map<String, Value>) {
+        state.insert(title.to_string(), json!(TaskStatus::PENDING.stringify()));
+        write_to_file("./state.json", state);
+        println!("\n\n{} is being set to pending\n\n", title);
     }
 }

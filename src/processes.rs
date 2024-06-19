319365c14c3bd70afc32a559054f5d19e3d48fb0
Zakaria <zakivic89@gmail.com>
2024-06-20 06:18:28 +0700

    added pending processor

---
+++ src/processes.rs
@@ -0,0 +1,23 @@
+use super::to_do::structs::done::Done;
+use super::to_do::structs::pending::Pending;
+use super::to_do::traits::create::Create;
+use super::to_do::traits::delete::Delete;
+use super::to_do::traits::edit::Edit;
+use super::to_do::traits::get::Get;
+use super::to_do::ItemTypes;
+use serde_json::value::Value;
+use serde_json::Map;
+
+fn process_pending(item: Pending, command: String, state: &Map<String, Value>) {
+    let mut state = state.clone();
+    match command.as_str() {
+        "get" => item.get(&item.super_struct.title, &state),
+        "create" => item.create(
+            &item.super_struct.title,
+            &item.super_struct.status.stringify(),
+            &mut state,
+        ),
+        "edit" => item.set_to_done(&item.super_struct.title, &mut state),
+        _ => println!("command: {} not supported", command),
+    }
+}

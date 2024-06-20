2ff1ba441b1e20c2e632d964f89331cb05aa9eba
Zakaria <zakivic89@gmail.com>
2024-06-21 05:51:04 +0700

    adjusted the done process for delete command

--- src/processes.rs
+++ src/processes.rs
@@ -26,12 +26,8 @@ fn process_done(item: Done, command: String, state: &Map<String, Value>) {
     let mut state = state.clone();
     match command.as_str() {
         "get" => item.get(&item.super_struct.title, &state),
-        "create" => item.create(
-            &item.super_struct.title,
-            &item.super_struct.status.stringify(),
-            &mut state,
-        ),
-        "edit" => item.set_to_done(&item.super_struct.title, &mut state),
+        "delete" => item.delete(&item.super_struct.title, &mut state),
+        "edit" => item.set_to_pending(&item.super_struct.title, &mut state),
         _ => println!("command: {} not supported", command),
     }
 }

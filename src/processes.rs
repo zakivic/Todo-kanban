f655e90515e3eb77dabf096961bb379dd01c99fd
Zakaria <zakivic89@gmail.com>
2024-06-21 05:44:05 +0700

    added process for done command

--- src/processes.rs
+++ src/processes.rs
@@ -21,3 +21,17 @@ fn process_pending(item: Pending, command: String, state: &Map<String, Value>) {
         _ => println!("command: {} not supported", command),
     }
 }
+
+fn process_done(item: Done, command: String, state: &Map<String, Value>) {
+    let mut state = state.clone();
+    match command.as_str() {
+        "get" => item.get(&item.super_struct.title, &state),
+        "create" => item.create(
+            &item.super_struct.title,
+            &item.super_struct.status.stringify(),
+            &mut state,
+        ),
+        "edit" => item.set_to_done(&item.super_struct.title, &mut state),
+        _ => println!("command: {} not supported", command),
+    }
+}

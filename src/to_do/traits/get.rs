207ddd3e15851d3cb7e511f280ae0f26a284f145
Zakaria <zakivic89@gmail.com>
2024-06-22 06:48:35 +0700

    fixed string ref issue

--- src/to_do/traits/get.rs
+++ src/to_do/traits/get.rs
@@ -2,7 +2,7 @@ use serde_json::value::Value;
 use serde_json::Map;
 pub trait Get {
     fn get(&self, title: &str, state: &Map<String, Value>) {
-        let item: Option<Value> = state.get("title");
+        let item: Option<&Value> = state.get("title");
 
         match item {
             Some(result) => {

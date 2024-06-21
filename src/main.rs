7e806f2dda071e136cc73a2e3dc7fe5c100db9ce
Zakaria <zakivic89@gmail.com>
2024-06-22 06:47:15 +0700

    updated main to use processes

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
+mod processes;
 mod state;
+mod to_do;
+use processes::process_input;
 use serde_json::value::Value;
-use serde_json::{json, Map};
-use state::{read_file, write_to_file};
+use serde_json::Map;
+use state::read_file;
 use std::env;
+use to_do::enums::TaskStatus;
+use to_do::to_do_factory;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let status: &String = &args[1];
+    let command: &String = &args[1];
     let title: &String = &args[2];
-    let mut state: Map<String, Value> = read_file("./state.json");
-    println!("Before operation: {:?}", state);
-    state.insert(title.to_string(), json!(status));
-    println!("After operation: {:?}", state);
-    write_to_file("./state.json", &mut state);
+    let state: Map<String, Value> = read_file("./state.json");
+    let status: String;
+    match &state.get(*&title) {
+        Some(result) => {
+            status = result.to_string().replace('\"', "");
+        }
+        None => {
+            status = "pending".to_owned();
+        }
+    }
+    let item = to_do_factory(title, TaskStatus::from_string(status.to_uppercase()));
+    process_input(item, command.to_string(), &state);
 }
